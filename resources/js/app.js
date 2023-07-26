7922304c2de359bfbd52ee223b8343f1ed4bfe7f
Mohamed Jebali <125953606+mohamed-jebali@users.noreply.github.com>
2023-07-26 19:19:32 +0200

    Initial commit

---
+++ resources/js/app.js
@@ -0,0 +1,7 @@
+import './bootstrap';
+import * as bootstrap from 'bootstrap';
+import '~resources/scss/app.scss';
+
+import.meta.glob([
+    '../img/**'
+])
---
+++ resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('title', 'Hello World')
+
+@section('main-content')
+    <h1 class='text-center'>
+       Hello World !
+    </h1>
+@endsection
